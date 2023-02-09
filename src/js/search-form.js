acfa956cb20ad797350d8360b89d423e19233b3d
monika-czulak <czulak.monika@gmail.com>
2023-02-09 18:51:09 +0100

    search-form.js

---
+++ src/js/search-form.js
@@ -0,0 +1,12 @@
+import { getSearchedMovies } from './fetch';
+
+const form = document.querySelector('#search-form');
+
+const handleSubmit = e => {
+  e.preventDefault();
+  const searchInput = e.target[0].value;
+  console.log(searchInput);
+  getSearchedMovies(searchInput).then(res => console.log(res.data.results));
+};
+
+form.addEventListener('submit', handleSubmit);
