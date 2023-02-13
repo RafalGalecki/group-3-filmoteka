585e77f6027b1d743a1593415d3a019ac1e76cd6
Marlena Dulęba <marlena_duleba@o2.pl>
2023-02-13 12:16:38 +0100

    switching headers using template

--- src/js/library.js
+++ src/js/library.js
@@ -1,47 +1,28 @@
-//import '../../css/index.min.css';
 import '../css/index.min.css';
 
 const headerHome = document.querySelector('.header-home');
-const home = document.querySelector('#home');
-const library = document.querySelector('#library');
-const form = document.querySelector('.search-form');
-const menu = document.querySelector('.menu');
-const headerHomeContainer = document.querySelector('.header-home__container');
+const libraryLink = document.querySelector('#library');
+const body = document.body;
 
-library.addEventListener('click', () => {
-//const bgrImage = '/src/images/bg-image.jpg';
-  headerHome.style.backgroundImage = `url(src/images/Rectangle-1x-desktop.jpg)`;
-  form.classList.add('visually-hidden');
-  const btnContainer = document.createElement('div');
-  btnContainer.innerHTML = '';
-  headerHomeContainer.append(btnContainer);
-  btnContainer.innerHTML = ` <div class="switch__btn">
-  <div class="switch__btn__icon-container">
-    <i class="switch__btn__icon fa-solid fa-sun"></i>
-  </div>
-</div>
 
-<ul class="header__btn-wrapper btn-wrapper js-library-controls">
-  <li>
-    <button
-      class="header__btn btn btn--white btn-js-active js-btn-watched"
-      type="button"
-    >
-      Watched
-    </button>
-  </li>
-  <li>
-    <button class="header__btn btn btn--white js-btn-qeue" type="button">
-      Queue
-    </button>
-  </li>
-</ul>`
+
+const libraryTemp = document.querySelector('.library-temp');
+const temp = libraryTemp.content.cloneNode(true);
+const homeLink = temp.querySelector('#home');
+console.log(temp);
+
+
+libraryLink.addEventListener('click', () => {
+    headerHome.classList.add('visually-hidden');
+    body.prepend(temp);
 });
 
-home.addEventListener('click', () => {
-    const bgrImage = '/src/images/bg-image.jpg';
-    headerHome.style.backgroundImage = `url(/src/images/bg-image.jpg)`;
-btnContainer.innerHTML = '';
-    form.classList.remove('visually-hidden');
-});
+homeLink.addEventListener('click', () => {
+    const headerLibrary = document.querySelector('.header-library');
+    headerHome.classList.remove('visually-hidden');
+    headerLibrary.remove();
+    
+} )
+
+
 
