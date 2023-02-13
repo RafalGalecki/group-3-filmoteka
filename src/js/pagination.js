9a6be4565a04d69d5bffdeb916ed229c34115dd3
Rafał Gałecki <darthunter@gmail.com>
2023-02-13 11:57:29 +0100

    Pagination arrow buttons work

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -9,7 +9,7 @@ export function renderCardPaginator(totalPages, selectedPage = 1) {
   const paginationContainer = document.getElementById('pagination-numbers');
   paginationContainer.innerHTML = '';
   const buttonsLimit = 9;
-  
+
   for (let i = 1; i <= pages; i++) {
     const pageBtn = document.createElement('button');
     pageBtn.setAttribute('type', 'button');
@@ -23,11 +23,52 @@ export function renderCardPaginator(totalPages, selectedPage = 1) {
     paginationContainer.append(pageBtn);
   }
 
+  //Arrow buttons prev and next
+  const prevBtn = document.createElement('button');
+  prevBtn.setAttribute('type', 'button');
+  prevBtn.setAttribute('value', '');
+  prevBtn.setAttribute('id', 'prevButton');
+  prevBtn.innerHTML = '&lt;';
+  prevBtn.style.backgroundColor = '#ff6b08';
+  paginationContainer.prepend(prevBtn);
+
+  const nextBtn = document.createElement('button');
+  nextBtn.setAttribute('type', 'button');
+  nextBtn.setAttribute('value', '');
+  nextBtn.setAttribute('id', 'nextButton');
+  nextBtn.innerHTML = '&gt;';
+  nextBtn.style.backgroundColor = '#ff6b08';
+  paginationContainer.append(nextBtn);
+
+  // on first search load (page 1) it makes button 1 active
   const pageBtnFirst = document.getElementById('1');
   pageBtnFirst.classList.add('activebtn');
 
   paginationContainer.addEventListener('click', event => {
     //event.preventDefault();
+    //disableButtons(prevBtn, nextBtn, selectedPage, totalPages);
+
+    if (event.target.id == 'prevButton') {
+      if (selectedPage === 1) {
+        return;
+      } else {
+        console.log('PREV', selectedPage);
+        selectedPage -= 1;
+        console.log('PREV after', selectedPage);
+        prevBtn.setAttribute('value', `${selectedPage}`);
+      }
+    }
+    if (event.target.id == 'nextButton') {
+      if (selectedPage === totalPages) {
+        return;
+      } else {
+        console.log('NEXT', selectedPage);
+        selectedPage += 1;
+        console.log('NEXT after', selectedPage);
+        nextBtn.setAttribute('value', `${selectedPage}`);
+      }
+    }
+
     selectedPage = Number(event.target.value);
 
     console.log('event.target.id', event.target.id);
@@ -68,10 +109,23 @@ export function renderCardPaginator(totalPages, selectedPage = 1) {
 
 // Page buttons logic
 
-
 function setActivePage(currentPage) {
   const elementActive = document.querySelector('.activebtn');
   elementActive.classList.remove('activebtn');
   const activeBtn = document.getElementById(`${currentPage}`);
   activeBtn.classList.add('activebtn');
+}
+
+function disableButtons(prevBtn, nextBtn, selectedPage, totalpages) {
+  //const prevBtn = document.getElementById('prevButton');
+  //const nextBtn = document.getElementById('nextButton');
+
+  prevBtn.setAttribute('disabled', false);
+  nextBtn.setAttribute('disabled', false);
+  if (selectedPage == 1) {
+    prevBtn.setAttribute('disabled', true);
+  }
+  if (selectedPage == totalpages) {
+    nextBtn.setAttribute('disabled', true);
+  }
 }
